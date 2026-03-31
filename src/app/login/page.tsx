fad68e5b250d4b6249ffe2c4a8fcea9cf371592e
Cu4lQu1er4 <correoquenuncauso123@gmail.com>
2026-03-31 12:26:53 -0500

    Error de redireccionamiento

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,15 +3,15 @@ import { getServerUser } from "@/lib/auth-server";
 import LoginForm from "./components/LoginForm";
 
 export default async function LoginPage() {
-  const user = await getServerUser();
+  // const user = await getServerUser();
 
-  if (user) {
-    if (user.role === "ADMIN") {
-      redirect("/admin");
-    } else {
-      redirect("/employee")
-    }
-  }
+  // if (user) {
+  //   if (user.role === "ADMIN") {
+  //     redirect("/admin");
+  //   } else {
+  //     redirect("/employee")
+  //   }
+  // }
 
   return <LoginForm />
 }
